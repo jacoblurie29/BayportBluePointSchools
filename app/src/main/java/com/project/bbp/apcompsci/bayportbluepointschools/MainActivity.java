c8f0e346cd09a7196c62136cee0f587b42531eb3
Jacob Lurie <160002440@bbplab.in>
2018-06-06 14:26:58 -0400

    Initial commit

---
+++ app/src/main/java/com/project/bbp/apcompsci/bayportbluepointschools/MainActivity.java
@@ -0,0 +1,46 @@
+package com.project.bbp.apcompsci.bayportbluepointschools;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+
+                    return true;
+                case R.id.navigation_eschool:
+
+                    return true;
+                case R.id.navigation_calendar:
+
+                    return true;
+                case R.id.navigation_teachers:
+
+                    return true;
+
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+}
